f1b5cd0c31a2da955bf90b3db1df6bbd45795084
ListenV <sky03.sen@outlook.com>
2025-09-29 08:12:00 +0800

    fix dhcp

--- LibWrt/100-custom.sh
+++ LibWrt/100-custom.sh
@@ -2,11 +2,14 @@
 
 uci batch << EOF
 
-set network.lan.ipaddr='10.1.1.1/24'
+del network.lan.ipaddr
+add_list network.lan.ipaddr='10.1.1.1/24'
 del network.wan6
 
 commit network
 
+del dhcp.lan.dhcp_option
+add_list dhcp.lan.dhcp_option='6,10.1.1.1'
 set dhcp.lan.start='50'
 set dhcp.lan.limit='51'
 
@@ -16,6 +19,8 @@ set system.@system[0].hostname='Router'
 
 set system.@system[0].zram_size_mb='128'
 set system.@system[0].zram_comp_algo='zstd'
+
+set system.@system[0].conloglevel='6'
 
 commit system
 
